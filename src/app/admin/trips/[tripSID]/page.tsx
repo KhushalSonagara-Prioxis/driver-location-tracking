edd00dfb4fa86acf56fe4687a0539aeb167c41eb
Kishanprioxis <Kishan@prioxis.com>
2025-09-03 17:36:21 +0530

    Added Map representation

--- src/app/admin/trips/[tripSID]/page.tsx
+++ src/app/admin/trips/[tripSID]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import GoogleMap from "@/components/GoogleMap";
-import { getLocationName } from "@/services/geocodingService";
+
 
 interface Trip {
   tripSID: string;
